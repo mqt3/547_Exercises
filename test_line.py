d5ffe7da8dcf2a4f9844b9d0573b23f2db3b169d
Michael Tian <Michaeltian77@gmail.com>
2021-09-13 23:47:50 -0400

    Wrote unit test for on_line function, testing 4 points

---
+++ test_line.py
@@ -0,0 +1,11 @@
+import pytest
+
+@pytest.mark.parametrize("point1, point2, x, expected",
+    [((0, 0), (5, 0), 10, 0), 
+    ((0, 0), (100, 100), 50, 50),
+    ((3, 5), (5, 10), 9, 20),
+    ((1.2, 5.25), (2.6, -5.75), 1.9, -0.25)])
+def test_on_line(point1, point2, x, expected):
+    from line import test_on_Line
+    result = on_line(point1, point2, x)
+    assert result == expected
